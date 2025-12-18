ff489aa079489143c21126db241c17f6a25195ff
YoshKoz <77861115+YoshKoz@users.noreply.github.com>
2025-12-18 22:14:12 +0100

    Echte eerste commit voor mijn Rust project

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let systeem = "Arch Linux";
+    let taal = "Rust";
+    
+    println!("Hallo vanaf {}!", systeem);
+    println!("Dit programma is geschreven in {}.", taal);
+    
+    for i in 1..=3 {
+        println!("Stap {} naar world domination...", i);
+    }
+}
