093a20d36c721d9c757049a1a6b235aa9422eb74
YoshKoz <77861115+YoshKoz@users.noreply.github.com>
2025-12-18 22:19:05 +0100

    Systeeminformatie toegevoegd aan main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use std::env;
+use std::process::Command;
+
 fn main() {
-    let systeem = "Arch Linux";
-    let taal = "Rust";
-    
-    println!("Hallo vanaf {}!", systeem);
-    println!("Dit programma is geschreven in {}.", taal);
-    
-    for i in 1..=3 {
-        println!("Stap {} naar world domination...", i);
-    }
+    // Haal de gebruikersnaam op uit de omgevingsvariabelen
+    let user = env::var("USER").unwrap_or_else(|_| String::from("onbekende gebruiker"));
+
+    // Voer het Linux 'uptime' commando uit
+    let uptime = Command::new("uptime")
+        .arg("-p")
+        .output()
+        .expect("Kon uptime niet ophalen");
+
+    let uptime_str = String::from_utf8_lossy(&uptime.stdout);
+
+    println!("---------------------------------------");
+    println!("Hallo, {}!", user);
+    println!("Je Arch WSL systeem is: {}", uptime_str.trim());
+    println!("Geprogrammeerd in Neovim!");
+    println!("---------------------------------------");
 }
---
+++ target/debug/hallo_rust.d
@@ -0,0 +1,1 @@
+/home/yoshkoz/dev/hallo_rust/target/debug/hallo_rust: /home/yoshkoz/dev/hallo_rust/src/main.rs
